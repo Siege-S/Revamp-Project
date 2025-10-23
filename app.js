ed4c71df74266e33bd8e483df775efa936dbebc5
Siege-S <sanchezchristianjoshua@gmail.com>
2025-10-23 09:52:28 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,69 @@
+const menuBtn = document.getElementById("menuBtn");
+const menu = document.getElementById("menu");
+const overlay = document.getElementById("overlay");
+const menuList = document.querySelectorAll(".menu-list");
+
+// Contact Button
+const contactNumber = "2069196886";
+function call() {
+  window.location.href = `tel:${contactNumber}`;
+}
+// Side Menu
+menuBtn.addEventListener("click", () => {
+  toggleMenu();
+});
+
+overlay.addEventListener("touchstart", () => {
+  toggleMenu();
+});
+
+function toggleMenu() {
+  if (menu.classList.contains("translate-x-full")) {
+    overlay.classList.remove("hidden");
+    menu.classList.remove("translate-x-full");
+    menu.classList.add("translate-x-0");
+  } else {
+    overlay.classList.add("hidden");
+    menu.classList.remove("translate-x-0");
+    menu.classList.add("translate-x-full");
+  }
+
+  console.log(menu);
+}
+
+menuList.forEach((item) => {
+  item.addEventListener("click", () => toggleMenu());
+});
+
+// Initialize Swiper
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  loop: true,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
+// Leaflet API
+var map = L.map("map").setView([36.208481695124156, -115.98891464631251], 13);
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 20,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+var marker = L.marker([36.2084, -115.9889]).addTo(map);
